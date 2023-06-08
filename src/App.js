e37054d3896f499de3f006dd7c122a271edb819a
roodhouse <rughjm@gmail.com>
2023-06-08 18:36:04 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="containerDiv" class="w-full h-screen bg-slate-200">
+        <div id="cardDiv">
+          <div id="imageDiv"></div>
+          <div id="textDiv">
+            <div id="headlineDiv">
+              <p>Improve your front-end skills by building projects</p>
+            </div>
+            <div id="sublineDiv">
+              <p>
+                Scan the QR code to visit Frontend Mentor and take your coding
+                skills to the next level
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="attribution text-[11px] text-center">
+          Challenge by{" "}
+          <a className='text-[#3e52a3]' href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a className='text-[#3e52a3]' href="https://github.com/roodhouse">roodhouse</a>.
+        </div>
+      </div>
     </div>
   );
 }
