1cc0f3571f78ecbd8c42b6c1d5123f572559a293
roodhouse <rughjm@gmail.com>
2023-06-08 19:49:08 -0500

    mobile view done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+import Image from './images/image-qr-code.png'
 
 function App() {
   return (
     <div className="App">
-      <div id="containerDiv" class="w-full h-screen bg-slate-200">
-        <div id="cardDiv">
-          <div id="imageDiv"></div>
-          <div id="textDiv">
-            <div id="headlineDiv">
-              <p>Improve your front-end skills by building projects</p>
+      <div id="containerDiv" className="w-full h-screen flex flex-col justify-center items-center bg-slate-200 px-[23px]">
+        <div id="cardDiv" className='flex flex-col items-center pt-[15px] px-[15px] rounded-[20px] bg-white mb-[50%]'>
+          <div id="imageDiv" className='pb-[35px]'>
+            <img src={Image} alt='qr code' className='rounded-[20px]'/>
+          </div>
+          <div id="textDiv" className='flex flex-col items-center justify-center'>
+            <div id="headlineDiv" className='flex justify-center pb-[15px]'>
+              <p className='font-black text-2xl'>Improve your front-end skills by building projects</p>
             </div>
-            <div id="sublineDiv">
-              <p>
+            <div id="sublineDiv" className='flex justify-center pb-[35px]'>
+              <p className='text-[hsl(212,45%,89%)] font-black text-[15px] max-w-[80%]'>
                 Scan the QR code to visit Frontend Mentor and take your coding
                 skills to the next level
               </p>
@@ -21,10 +24,10 @@ function App() {
 
         <div className="attribution text-[11px] text-center">
           Challenge by{" "}
-          <a className='text-[#3e52a3]' href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          <a className='text-[hsl(228,45%,44%)]' href="https://www.frontendmentor.io?ref=challenge" target="_blank">
             Frontend Mentor
           </a>
-          . Coded by <a className='text-[#3e52a3]' href="https://github.com/roodhouse">roodhouse</a>.
+          . Coded by <a className='text-[hsl(228,45%,44%)]' href="https://github.com/roodhouse">roodhouse</a>.
         </div>
       </div>
     </div>
